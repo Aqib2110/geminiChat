6688e8a569d93db5fc80dc627ef98b26b74238db
Aqib2110 <muhammadaqib2110@gmail.com>
2025-03-17 19:57:01 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,164 @@
+import React, { useState,useEffect } from "react";
+import axios from "axios";
+import Image from "./components/image";
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [responsede, setResponsede] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+  const data =  localStorage.getItem('items');
+  if(data){
+    const parsedata = JSON.parse(data);
+    setResponsede(parsedata);
+  }
+  else{
+    setResponsede([]); 
+  }
+  }, [])
+  useEffect(() => {
+    if(responsede.length > 0)
+    {
+      localStorage.setItem('items',JSON.stringify(responsede));
+    }
+
+  }, [responsede])
+  
+  const handleGenerate = async () => {
+    if (!query) return alert("Please enter a query!");
+    
+    setLoading(true);
+    try {
+      const res = await axios.post("http://localhost:3000/ask", { query });
+      setQuery("");
+      const id = Math.random();
+      const obj = {id:id,query:query,data:res.data.response};
+      setResponsede(arr=>[...arr,obj]);
+    } catch (error) {
+      setResponsede("Error fetching response!");
+    }
+    setLoading(false);
+  };
+
+  return (
+    <div style={{border:"white",
+      height:"100vh",
+    }} className="">
+      <div style={{
+       height:"90vh",
+      
+      }} className="">
+       <center style={{
+        border:"white",
+       }}>
+       <h1 style={{color:"white",
+     padding:"20px 0"
+      
+       }} className="text-2xl font-bold text-center mb-4">Gemini Chat</h1>
+        </center> 
+      
+          <div style={{
+            display:"flex",
+            flexDirection:"column",
+            "gap":"10px",
+          height:"75vh",
+            // border:"white",
+            overflowY:"scroll",
+          
+            
+            // backgroundColor:"white"
+          }} className="mt-4 p-3 bg-gray-200 rounded-lg div-res">
+
+{responsede.length > 0 ? responsede.map(data=>{ return data.imageUrl ?  <div style={{padding:"0px 20px"}}><img  src={data.imageUrl} height="100%" width="70%"  />
+
+</div>  : <div key={data.id}>
+<div style={{"display":"flex",
+  "justifyContent":"end",
+    padding:"3px 20px"
+}}>
+<strong  style={
+{
+"borderRadius":"10px",
+  "backgroundColor":"#303030",
+  "color":"white",
+  "padding":"10px"
+}
+}
+
+>{data.query}</strong>
+</div>
+<div style={
+  {
+     padding:"3px 20px"
+  }
+}>
+<strong style={
+  {
+    "color":"white",
+  }
+}>{data.data}</strong>
+</div>
+</div> 
+        
+         
+      
+
+  
+
+
+
+
+
+}) :<div></div>}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         </div>
+     
+      
+        
+      
+     
+      </div>
+      <div className="main" >
+        <textarea
+          className="w-full text p-3 border border-gray-300 rounded-lg"
+          rows="3"
+          placeholder=" Ask Anything..."
+          style={{marginBottom:0}}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          required
+        />
+          <button
+          className="btn"
+          onClick={handleGenerate}
+          disabled={loading}
+        >
+          {loading ? "Generating..." : "Generate"}
+        </button>
+        </div>
+      <center>
+      {/* <Image setResponsede = {setResponsede}/> */}
+        </center> 
+
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Image.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function Image({setResponsede}) {
+  const [image, setImage] = useState('');
+  const [prompt, setPrompt] = useState('');
+
+  const generateImage = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/generate-image', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ prompt })
+      });
+      
+      const data = await response.json();
+      setImage(data.imageUrl);
+      setResponsede(initial=>[...initial,data]);
+      console.log(data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <textarea
+        value={prompt}
+        className='text'
+        rows={5}
+        onChange={(e) => setPrompt(e.target.value)}
+        placeholder="Enter prompt"
+      />
+      <button className='btn' onClick={generateImage}>Generate</button>
+      
+     
+    </div>
+  );
+}
+
+export default Image;
